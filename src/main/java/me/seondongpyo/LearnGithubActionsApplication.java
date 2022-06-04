f263fe46505ec12baec85f6ed600d4b874a2f38e
seondongpyo <seondongpyo@gmail.com>
2022-06-04 22:23:48 +0900

    initial commit

---
+++ src/main/java/me/seondongpyo/LearnGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package me.seondongpyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnGithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/seondongpyo/LearnGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seondongpyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
