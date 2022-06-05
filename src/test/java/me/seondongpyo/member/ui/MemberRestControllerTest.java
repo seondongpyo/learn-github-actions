d69119399bfec1c0bcdd61f1d392c28fff9b002f
seondongpyo <seondongpyo@gmail.com>
2022-06-05 13:46:44 +0900

    회원 삭제 테스트를 실패하도록 예외 클래스 변경

--- src/test/java/me/seondongpyo/member/ui/MemberRestControllerTest.java
+++ src/test/java/me/seondongpyo/member/ui/MemberRestControllerTest.java
@@ -94,6 +94,6 @@ class MemberRestControllerTest {
 			.andExpect(status().isNoContent());
 
 		assertThatThrownBy(() -> memberService.findById(id))
-			.isInstanceOf(IllegalArgumentException.class);
+			.isInstanceOf(IllegalStateException.class);
 	}
 }
