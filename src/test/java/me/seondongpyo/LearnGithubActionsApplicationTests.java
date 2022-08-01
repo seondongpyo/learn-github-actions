a54f2d1ebb90e2fb88cb069ff7b003f52f9b2950
seondongpyo <seondongpyo@gmail.com>
2022-08-01 23:45:32 +0900

    feat: application.properties에 추가한 변수로 테스트

--- src/test/java/me/seondongpyo/LearnGithubActionsApplicationTests.java
+++ src/test/java/me/seondongpyo/LearnGithubActionsApplicationTests.java
@@ -3,10 +3,12 @@ package me.seondongpyo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@PropertySource("classpath:application.properties")
 class LearnGithubActionsApplicationTests {
 
 	@Value("${test.variable}")
