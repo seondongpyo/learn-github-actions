6f399acea8f027edab7d972cbeb58e521dabcfef
seondongpyo <seondongpyo@gmail.com>
2022-08-02 00:24:46 +0900

    환경변수 변경

--- src/test/java/me/seondongpyo/LearnGithubActionsApplicationTests.java
+++ src/test/java/me/seondongpyo/LearnGithubActionsApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.PropertySource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@PropertySource("classpath:/application.properties")
+@PropertySource("classpath:application.properties")
 class LearnGithubActionsApplicationTests {
 
 	@Value("${test.variable}")
@@ -16,7 +16,7 @@ class LearnGithubActionsApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertThat(variable).isEqualTo("secrets test");
+		assertThat(variable).isEqualTo("property test");
 	}
 
 }
