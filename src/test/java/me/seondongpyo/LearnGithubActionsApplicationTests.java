5a1969bd766675afef3d14618d309d9186ccf241
seondongpyo <seondongpyo@gmail.com>
2022-08-01 23:19:09 +0900

    test: Github Actions secrets로 환경변수 테스트

--- src/test/java/me/seondongpyo/LearnGithubActionsApplicationTests.java
+++ src/test/java/me/seondongpyo/LearnGithubActionsApplicationTests.java
@@ -1,13 +1,21 @@
 package me.seondongpyo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class LearnGithubActionsApplicationTests {
 
+	@Value("${github.actions.variable}")
+	private String variable;
+
 	@Test
 	void contextLoads() {
+		assertThat(variable).isEqualTo("Github Actions");
 	}
 
 }
